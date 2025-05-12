fc93979f534d7b914fbc2e42e2b2089e55c05d87
Lebackrobot <lebackman18@gmail.com>
2025-05-12 20:20:52 -0300

    ✨ feat: main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,21 @@
-#include "header.hpp"
+#include <Arduino.h>
+#include "LcdController.hpp"
 
-void setup() {}
-void loop() {} 
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+LcdController lcdController(&lcd);
+
+void setup() {
+    lcdController.init();
+}
+
+void loop() {
+    lcdController.printInRow1("Hello, World!");
+    
+    delay(1000);
+    lcdController.clear();
+    
+    lcdController.printInRow2("Hello, World!");
+   
+    delay(1000);
+    lcdController.clear();
+} 
